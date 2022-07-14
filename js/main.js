b91492f524371225a3f2a974a901d172f7569e56
Washington França <93933244+washfranca@users.noreply.github.com>
2022-07-14 20:39:18 -0300

    comit variaveis

---
+++ js/main.js
@@ -0,0 +1,37 @@
+
+var frutas = [{nome: "maça", cor:"vermelha"}, {nome: "uva", cor:"roxa"}]   
+console.log(frutas);
+alert(frutas[1].nome);
+
+//var fruta = {nome: "maça", cor:"vermelha"}
+//console.log(fruta.nome);
+//alert(fruta.cor);
+
+
+//var nome = "Washington Luiz";
+ //var idade = 50;
+ //var idade2 = 40;
+ //var frase = "Japão é a Melhor seleção do Mundo"
+//alert(nome + " tem " + idade + " anos ");
+//alert(idade + idade2);
+
+  //console.log(nome);
+  //console.log(idade + idade2);
+  //console.log(frase.replace("Japão", "Brasil"));
+ //alert(frase.replace("Japão", "Brasil"));
+
+
+ //var lista = ["maça", "banana", "laranja"];
+ //lista.push("uva");
+ //lista.pop();
+ //console.log(lista.length);
+ //console.log(lista.reverse());
+ // console.log(lista);
+ //console.log(lista.toString());
+ //console.log(lista.join(" - "));
+
+
+ 
+
+
+
