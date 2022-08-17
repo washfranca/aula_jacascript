9e62fe55cb43aa80ddf4283ae4aa8d49c078576a
Washington França <93933244+washfranca@users.noreply.github.com>
2022-08-17 14:49:08 -0300

    commit variaveis

--- js/main.js
+++ js/main.js
@@ -5,6 +5,19 @@ function clicou(){
   //console.log(document.getElementById("agradecimento"));
 }
 
+function redirecionar (){
+  window.open ("https://globalacademy.globalsys.com.br/");
+
+}
+
+function trocar(){
+  document.getElementById("mousemove").innerHTML = "Obrigado por passar o mouse";
+ // alert("trocar texto");
+}
+function voltar(){
+  document.getElementById("mousemove").innerHTML = "Passe o mouse aqui";
+}
+
 
 
 
